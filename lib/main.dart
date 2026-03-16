bf436f8ee458e0d0231f962a7d2f00cf59e9ae82
ahnsihyun000 <lampe2003@gmail.com>
2026-03-17 01:17:25 +0900

    첫 번째 QR 스캐너 코드 업로드

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void main() {
+  runApp(const MaterialApp(home: QRScannerScreen()));
+}
+
+class QRScannerScreen extends StatefulWidget {
+  const QRScannerScreen({super.key});
+
+  @override
+  State<QRScannerScreen> createState() => _QRScannerScreenState();
+}
+
+class _QRScannerScreenState extends State<QRScannerScreen> {
+  String? qrCode;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('시현 팀장의 QR 출석체크')),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 5,
+            child: MobileScanner(
+              onDetect: (capture) {
+                final List<Barcode> barcodes = capture.barcodes;
+                for (final barcode in barcodes) {
+                  setState(() {
+                    qrCode = barcode.rawValue;
+                  });
+                }
+              },
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              alignment: Alignment.center,
+              color: Colors.white,
+              child: Text(
+                qrCode != null ? '스캔 결과: $qrCode' : 'QR 코드를 스캔해주세요!',
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
