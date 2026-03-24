5581309e7f04bc98232072c2874bae01e458a504
2237020min <2237020@pcu.ac.kr>
2026-03-24 18:21:45 +0900

    03.24(2)

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../models/attendance_model.dart';
-import '../models/user_model.dart';
 
 class DatabaseService {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
@@ -9,7 +8,7 @@ class DatabaseService {
   Future<String> recordAttendance(AttendanceModel attendance) async {
     try {
       // 1. 고유 문서 ID 생성: '행사ID_사용자ID' 조합 
-      // 이렇게 하면 한 사람이 같은 행사에 여러 번 스캔해도 DB에는 하나만 남습니다.
+      // 한 사람이 같은 행사에 여러 번 스캔해도 DB에는 하나만 남습니다.
       String customDocId = "${attendance.eventId}_${attendance.userUid}";
       
       // 2. 이미 출석했는지 문서 존재 여부 확인 
@@ -35,23 +34,5 @@ class DatabaseService {
         .map((snapshot) => snapshot.docs
             .map((doc) => AttendanceModel.fromFirestore(doc))
             .toList());
-  }
-  // 학번(studentId)으로 사용자 정보 가져오기 함수 추가
-  Future<UserModel?> getUserData(String studentId) async {
-    try {
-      // 'users' 컬렉션에서 문서 ID가 스캔한 학번인 것을 찾습니다.
-      DocumentSnapshot doc = await _db.collection('users').doc(studentId).get();
-
-      if (doc.exists) {
-        // 데이터가 있으면 UserModel 객체로 변환
-        return UserModel.fromMap(doc.data() as Map<String, dynamic>);
-      } else {
-        print("사용자를 찾을 수 없습니다.");
-        return null;
-      }
-    } catch (e) {
-      print("사용자 조회 에러: $e");
-      return null;
-    }
   }
 }
